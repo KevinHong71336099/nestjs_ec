b7867615914956681088f8a1dde8965d6f5171fd
KevinHong71336099 <kevin71336099@gmail.com>
2024-10-06 15:19:28 +0800

    feat: add redis module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,14 @@ import { UsersModule } from './users/users.module';
 import dataSourceOptions from './global/config/database/dataSource';
 import { LoggerModule } from 'nestjs-pino';
 import { loggerConfig } from './global/config/pino/logger.config';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({ ...dataSourceOptions, autoLoadEntities: true }),
     LoggerModule.forRoot(loggerConfig),
     UsersModule,
+    RedisModule,
   ],
   controllers: [AppController],
   providers: [AppService],
